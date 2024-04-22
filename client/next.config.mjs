142f19c030c56260641409f5f89d0f13bac3d450
Wasim Akram <mr.saim516@gmail.com>
2024-04-23 01:06:58 +0500

    Getting started with Next js

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      API_URL: 'http://localhost:5000', // Replace with your server URL
+    },
+    // ... other configurations
+  };
+  
+  export default nextConfig;
+  
